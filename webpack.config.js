60ce5a8bf107e19457f97ebb824f242dc55e3058
周逸群 <2938914315@qq.com>
2019-03-19 10:42:53 +0800

    添加自动修复

--- webpack.config.js
+++ webpack.config.js
@@ -10,19 +10,6 @@ module.exports = {
     filename: '[name].bundle.js'
   },
   module: {
-    rules: [{
-      enforce: 'pre',
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'eslint-loader',
-      options: {
-        fix: true
-      }
-    },
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    }]
+    rules: []
   }
 }
