e814e20a304f0b5a5cd4386b028a375b11729e5c
周逸群 <2938914315@qq.com>
2019-03-19 10:36:55 +0800

    提交正确的版本

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path')
 
 module.exports = {
+  mode: 'development',
   entry: {
-    app: 'index.js'
+    app: './src/index.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -13,7 +14,10 @@ module.exports = {
       enforce: 'pre',
       test: /\.js$/,
       exclude: /node_modules/,
-      loader: 'eslint-loader'
+      loader: 'eslint-loader',
+      options: {
+        fix: true
+      }
     },
     {
       test: /\.js$/,
